43d370676e7060ecafa021987beab8b692b5e6db
Durga R <137531455+durga-164@users.noreply.github.com>
2023-12-07 22:24:05 +0530

    ItemController.java

--- SpringBootRestApiProject/src/main/java/com/tns/restapi/entity/controller/ItemController.java
+++ SpringBootRestApiProject/src/main/java/com/tns/restapi/entity/controller/ItemController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+    @CrossOrigin(origins = "http://localhost:3000") // Adding @CrossOrigin at class level to cover all mappings
 public class ItemController {
 
     private final ItemService itemService;
